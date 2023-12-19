9ff105ab4f33a1940e200634fefe7000bcceb76a
praseen <user@DESKTOP-7G5QTO4>
2023-12-19 11:17:22 +1100

    api component is functional

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+//import Quote from './quote.js'
+//import Zquote from './zenquote.js'
+import TypeQuote from './api';
+//Please add <TypeQuote /> component in the return div of App() function
 
 function App() {
   return (
     <div className="App">
-
+       
     </div>
   );
 }
