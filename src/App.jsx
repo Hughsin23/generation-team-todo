35efaa506034422937c7e1b1e9c9bcc083e87137
Hughsin23 <hughsy_23@hotmail.com>
2023-12-19 13:05:42 +1100

    finishing logic, moving to building the form itself

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Form from './Form.jsx'
 import TypeQuote from './api';
 //Please add <TypeQuote /> component in the return div of App() function
 
+
 function App() {
   return (
     <div className="App">
--- src/Form.jsx
+++ src/Form.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react'
-import todoShape from './todo.js'
+import toDoShape from './todo.js'
 
 const Form = ({ toDoID }) => {
   // this is grabbing the initial shape I made in the task.js file as the default shape for our toDo task, I used the spread operator to make sure it was making a clone that is mutable rather than it editing a shallow copy of the original object.
   const [toDo, setToDo] = useState({ ...toDoShape })
-  const [toDoList, setToDoList] = usestate([])
+  const [toDoList, setToDoList] = useState([])
   const [errorMessage, setErrorMessage] = useState('')
 
   // have a useeffect to grab the stored toDos 
@@ -36,7 +36,7 @@ const Form = ({ toDoID }) => {
     // check if the todo already exists through the ID we pass into the component
     if (toDoID) {
       const updatedTodoList = toDoList.map((task) => {
-        task.id === toDoID ? toDo : task
+        return task.id === toDoID ? toDo : task
       })
       setToDoList(updatedTodoList)
     } else { // add a new task if it doesn't exist already, using the date as an ID.
@@ -44,7 +44,7 @@ const Form = ({ toDoID }) => {
       setToDoList((prevToDoList) => [...prevToDoList, newToDo])
     }
     // reset the todo after submitting the current one, as well as resetting the error.
-    setToDo({ ...todoShape })
+    setToDo({ ...toDoShape })
     setErrorMessage('')
 
   }
@@ -56,7 +56,60 @@ const Form = ({ toDoID }) => {
   }
 
   const handleToDoStatusChange = (toDoID, newToDoStatus) => {
-
+    const updatedToDoList = toDoList.map((task) => {
+      return task.id === toDoID ? { ...task, status: newToDoStatus } : task
+    })
+    setToDoList(updatedToDoList)
   }
+
+  const renderToDoList = () => {
+    const sortedToDoList = toDoList.sort((a, b) => a.status.localeCompare(b.status))
+
+    return sortedToDoList.map((task) => {
+      <div className="todo-container">
+        <h2 className="todo-name">{task.toDoName}</h2>
+        <h2 className="todo-description">{task.toDoDescription}</h2>
+        <h2 className="todo-dueDate">{task.dueDate}</h2>
+        <h2 className="todo-assignedMember">{task.assignedMember}</h2>
+        <h2 className="todo-status">{task.toDoStatus}</h2>
+        <button className="in-progress" onClick={() => handleToDoStatusChange(task.id, 'in-progress')}></button>
+        <button className="completed" onClick={() => handleToDoStatusChange(task.id, 'completed')}></button>
+        <button className="review" onClick={() => handleToDoStatusChange(task.id, 'review')}></button>
+        <button className="remove" onClick={() => handleRemove(task.id)}></button>
+      </div>
+    })
+  }
+
+
+  return (
+    <>
+      <form action="" id="todo-form">
+        <label for="name">Todo name: </label>
+        <input type="text" name="name" value={toDo.toDoName} onChange={handleChange} required />
+        <br />
+
+
+        <label for="description">Todo description: </label>
+        <input type="text" name='description' />
+        <br />
+
+        <label for="due-date">Todo due date: </label>
+        <input type="text" name='due-date' />
+        <br />
+
+        <label for="member">Todo assignmed member: </label>
+        <input type="text" name='member' />
+        <br />
+
+        <label for="status">Todo status:</label>
+        <select name="status">
+          <option value="in-progress">In Progress</option>
+          <option value="completed">Completed</option>
+          <option value="review">Up for Review</option>
+        </select>
+      </form>
+
+    </>
+  )
 
 }
